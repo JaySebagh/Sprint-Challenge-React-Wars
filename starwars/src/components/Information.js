ace4b442958eca06bb3a1bd0a439ca097e0de886
JaySebagh <jaysebagh@gmail.com>
2019-02-15 10:23:13 -0800

    added more info

--- starwars/src/components/Information.js
+++ starwars/src/components/Information.js
@@ -3,7 +3,14 @@ import React from "react";
 const Information = props => {
     return (
         <div>
-            <h1>{props.characters.name}</h1>
+            <h1>Name: {props.characters.name}</h1>
+            <p>Height: {props.characters.height}</p>
+            <p>Mass: {props.characters.mass}</p>
+            <p>Hair Color: {props.characters.hair_color}</p>
+            <p>Skin Color: {props.characters.skin_color}</p>
+            <p>Eye Color: {props.characters.eye_color}</p>
+            <p>Birth Year: {props.characters.birth_year}</p>
+            <p>Gender: {props.characters.gender}</p>
         </div>
     )
 }
